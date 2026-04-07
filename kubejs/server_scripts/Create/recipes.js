99488234d2beb8ac9a98df659e561daa2e4c8d88
Sylvia-Olliff <sylviaolliff@gmail.com>
2026-04-07 00:26:14 -0400

    Additional Create recipes

--- kubejs/server_scripts/Create/recipes.js
+++ kubejs/server_scripts/Create/recipes.js
@@ -1,6 +1,10 @@
 ServerEvents.recipes(evt => {
     const create = CreateRecipeGenerator(evt);
 
+    /**
+     * Crushing recipes for Deeper and Darker ores. Each ore has a chance to yield extra resources, with the probabilities increasing for higher-tier ores. 
+     * The base output is 2 units of the corresponding resource, with chances for additional outputs based on the ore type.
+     */
     create.crushing()
         .itemIn('deeperdarker:gloomslate_coal_ore')
         .itemOut('minecraft:coal')
@@ -15,4 +19,39 @@ ServerEvents.recipes(evt => {
         .itemOut('minecraft:raw_iron', 0.3)
         .itemOut('minecraft:raw_iron', 0.1)
         .id('nerdhaus:create/crushing/gloomslate_iron_ore');
+
+    create.crushing()
+        .itemIn('deeperdarker:gloomslate_gold_ore')
+        .itemOut('minecraft:raw_gold')
+        .itemOut('minecraft:raw_gold')
+        .itemOut('minecraft:raw_gold', 0.3)
+        .itemOut('minecraft:raw_gold', 0.1)
+        .id('nerdhaus:create/crushing/gloomslate_gold_ore');
+
+    create.crushing()
+        .itemIn('deeperdarker:gloomslate_copper_ore')
+        .itemOut('minecraft:raw_copper')
+        .itemOut('minecraft:raw_copper')
+        .itemOut('minecraft:raw_copper', 0.3)
+        .itemOut('minecraft:raw_copper', 0.1)
+        .id('nerdhaus:create/crushing/gloomslate_copper_ore');
+
+    create.crushing()
+        .itemIn('deeperdarker:gloomslate_redstone_ore')
+        .itemOut('minecraft:redstone')
+        .itemOut('minecraft:redstone')
+        .itemOut('minecraft:redstone')
+        .itemOut('minecraft:redstone')
+        .itemOut('minecraft:redstone', 0.3)
+        .itemOut('minecraft:redstone', 0.1)
+        .id('nerdhaus:create/crushing/gloomslate_redstone_ore');
+    
+    create.crushing()
+        .itemIn('deeperdarker:gloomslate_lapis_ore')
+        .itemOut('minecraft:lapis_lazuli')
+        .itemOut('minecraft:lapis_lazuli')
+        .itemOut('minecraft:lapis_lazuli')
+        .itemOut('minecraft:lapis_lazuli', 0.3)
+        .itemOut('minecraft:lapis_lazuli', 0.1)
+        .id('nerdhaus:create/crushing/gloomslate_lapis_ore');
 });
