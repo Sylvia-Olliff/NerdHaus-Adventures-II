6eec58527376ca9dd3b355ac93abe4bf22b0291a
Sylvia-Olliff <sylviaolliff@gmail.com>
2026-04-25 12:13:39 -0400

    gitignore cleanup. Added script for removing recipes from EMI

---
+++ kubejs/server_scripts/Create/emiRemoval.js
@@ -0,0 +1,12 @@
+RecipeViewerEvents.removeEntries('item', event => {
+    event.remove('createfood:apple_slice');
+    event.remove('createfood:butterscotch_apple_slice');
+    event.remove('createfood:caramel_apple_slice');
+    event.remove('createfood:chocolate_apple_slice');
+    event.remove('createfood:creme_brulee_bottle');
+    event.remove('createfood:dark_chocolate_apple_slice');
+    event.remove('createfood:honeyed_apple_slice');
+    event.remove('createfood:toffee_apple_slice');
+    event.remove('createfood:white_chocolate_apple_slice');
+    event.remove('createfood:creme_brulee_bottle_block');
+});
